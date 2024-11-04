0b604e8cada06b2302443cedcf6f2a11b07e935f
Bongani M. Ndhlovu <bongani.ndhlovu@4-sure.net>
2024-11-05 00:15:40 +0200

    fix(Student): fixed the Student db name issue

--- student/models.py
+++ student/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Student(db.Model):
-    __tablename__ = 'student'
+    __tablename__ = 'Student'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
